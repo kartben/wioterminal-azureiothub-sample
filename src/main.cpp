d101761f09559b69d1e2621a2e2cb5a18ba6ea20
Takashi Matsuoka <matsujirushi@live.jp>
2020-11-15 11:34:48 +0900

    Retry WiFi.begin() if not connected

--- src/main.cpp
+++ src/main.cpp
@@ -463,13 +463,13 @@ void setup()
     // Connect Wi-Fi
 
     DisplayPrintf("Connecting to SSID: %s", IOT_CONFIG_WIFI_SSID);
-    WiFi.begin(IOT_CONFIG_WIFI_SSID, IOT_CONFIG_WIFI_PASSWORD);
-
-    while (WiFi.status() != WL_CONNECTED)
+    do
     {
         Log(".");
-        delay(1000);
+        WiFi.begin(IOT_CONFIG_WIFI_SSID, IOT_CONFIG_WIFI_PASSWORD);
+        delay(500);
     }
+    while (WiFi.status() != WL_CONNECTED);
     DisplayPrintf("Connected");
 
     ////////////////////
